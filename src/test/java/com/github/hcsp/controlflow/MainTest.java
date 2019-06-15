01f1fb06a361e46bc6f52ef34e6aa051f57987c7
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:08 +0800

    Update repo

--- src/test/java/com/github/hcsp/controlflow/MainTest.java
+++ src/test/java/com/github/hcsp/controlflow/MainTest.java
@@ -1,16 +1,15 @@
 package com.github.hcsp.controlflow;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.util.Random;
+import org.apache.commons.math3.util.CombinatoricsUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class MainTest {
+public class MainTest {
     @Test
     public void test() {
-        int n = new java.util.Random().nextInt(10);
-        assertEquals(
-                (int) org.apache.commons.math3.util.CombinatoricsUtils.factorial(n),
-                Main.factorial(n));
-        assertEquals(1, Main.factorial(0));
+        int n = new Random().nextInt(10);
+        Assertions.assertEquals((int) CombinatoricsUtils.factorial(n), Main.factorial(n));
+        Assertions.assertEquals(1, Main.factorial(0));
     }
 }
