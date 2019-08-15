75b849376259b787c40739735f89875801d1e66e
王杰 <46479343+wangjie-fourth@users.noreply.github.com>
2019-08-16 00:15:29 +0800

    递归求阶乘 (#46)
    递归求阶乘 (#46)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,11 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        if (n == 0){
+            return 1;
+        }else {
+            return n * factorial(n - 1);
+        }
+    }
 }
