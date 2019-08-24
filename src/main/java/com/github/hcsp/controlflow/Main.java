ff51d67ec72869ba0d391e445a58e497de0898ad
杨汉杰 <36122057+freshmanyang@users.noreply.github.com>
2019-08-24 19:29:40 -0400

    循环计算阶乘 (#49)
    循环计算阶乘 (#49)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,13 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            n *= i;
+        }
+        return n;
+    }
 }
