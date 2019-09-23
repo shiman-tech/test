cf9c007f8cfce157a4fd838fdb4f55bf72bb5b13
Yuanmeng Xia <xymreasonxxs@163.com>
2019-09-23 20:15:59 +0800

    计算阶乘 (#65)
    计算阶乘 (#65)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,13 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        int sum = 1;
+        if (n == 0)
+            return sum;
+        for (int i = 1; i < n + 1; i++) {
+            sum *= i;
+        }
+        return sum;
+    }
 }
