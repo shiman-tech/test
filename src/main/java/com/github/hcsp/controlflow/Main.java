faf2d00d01d0e43322511d4348d8d1cbdb89ee2b
Lian Yongbo <L1anyX@126.com>
2019-10-22 22:07:37 +0800

    编写了阶乘方法 (#80)
    编写了阶乘方法 (#80)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,14 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        int result = 1;
+        if (n == 0) {
+        } else {
+            for (int i = 2; i <= n; i++) {
+                result *= i;
+            }
+        }
+        return result;
+    }
 }
