d09ff4c6bfa5e1d51fff7c782b045c0c73b92a2c
linsinan1995 <47880367+linsinan1995@users.noreply.github.com>
2019-12-14 23:30:56 +0100

    factorial function (#100)
    factorial function (#100)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,8 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        if (n==0) return 1;
+        return factorial(n-1) * n;
+    }
 }
