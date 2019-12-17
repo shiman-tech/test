7512726c3a99ed1dd1fe19608a8cab48a6b02fc1
astak16 <1500846601@qq.com>
2019-12-17 18:39:50 +0800

    计算阶乘 (#102)
    计算阶乘 (#102)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,11 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        int count = 1;
+        for (int i = 1; i <= n; i++) {
+            count *= i;
+        }
+        return count;
+    }
 }
