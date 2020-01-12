78d6761123c0476d65bc4483ef44b8f7ac288ff0
dygalo <59307187+dygalo@users.noreply.github.com>
2020-01-12 19:34:22 +0800

    编写阶乘函数 (#120)
    编写阶乘函数 (#120)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,15 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        int sum = 1;
+        if (n == 0)
+            return 1;
+        if (n == 1) {
+            return 1;
+        } else {
+            sum = n * factorial(n - 1);
+            return sum;
+        }
+    }
 }
