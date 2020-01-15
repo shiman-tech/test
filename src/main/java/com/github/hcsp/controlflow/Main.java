f742a026a3d7dbaf24c969073c59bd4363a8d5a5
mysteryven <33973865+mysteryven@users.noreply.github.com>
2020-01-15 15:17:56 +0800

    递归求阶乘 (#122)
    递归求阶乘 (#122)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,18 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        return computeFactorial(n);
+    }
+
+    public static int computeFactorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * computeFactorial(n - 1);
+    }
 }
