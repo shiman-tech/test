072c7fcf07152705fb28156acf142303ca8fe8ab
YingHuaLiu <35859346+YingHuaLiu@users.noreply.github.com>
2020-01-30 13:33:38 +0800

    求阶乘 (#127)
    求阶乘 (#127)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,19 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (n > 1) {
+            int i = n;
+            do {
+                i--;
+                n *= i;
+            }
+            while (i > 1);
+            return n;
+        }
+        return 0;
+    }
 }
