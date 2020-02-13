7922f896602d786540d5a8153b41d904880a41aa
JinbingLin <59944908+352503538@users.noreply.github.com>
2020-02-13 15:23:40 -0800

    使用循环，计算一个数的阶乘 (#134)
    使用循环，计算一个数的阶乘 (#134)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,12 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        int sum = 1;
+        if (n == 0) return sum;
+        for (int i = 1; i <= n; i++) {
+            sum *= i;
+        }
+        return sum;
+    }
 }
