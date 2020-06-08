817029b7609182836cb318c6e40fa5488a5aba5b
左佳宾 <59503711+keda725@users.noreply.github.com>
2020-06-08 22:50:31 +0800

    阶乘 (#188)
    阶乘 (#188)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,11 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        int num = 1;
+        for (int i = n; i > 0; i--) {
+            num *= i;
+        }
+        return num;
+    }
 }
