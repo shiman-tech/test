b60772796d37c903b58cbd324ba87cc7a2f5d545
linweikun95 <63492316+linweikun95@users.noreply.github.com>
2020-06-20 15:14:23 +0800

    fix: complete factorial (#191)
    fix: complete factorial (#191)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,11 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        int acc = 1;
+        for (int i = n; i > 0; i--) {
+            acc = acc * i;
+        }
+        return acc;
+    }
 }
