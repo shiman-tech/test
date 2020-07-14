cc789241162182a458c0de73e2a722014bc54225
dadong1196 <68277736+dadong1196@users.noreply.github.com>
2020-07-14 20:06:54 +0800

    阶乘 (#196)
    阶乘 (#196)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,23 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int b = 1;
+
+        for (int i = 0; i < n; i++) {
+            b = (i+ 1) * b;
+        }
+//        for (int i = 0; i < n; i++) {
+//            m = (i + 1) * m;
+//        }
+
+        return b;
+
+
+    }
+
+
 }
