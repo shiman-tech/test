50b0d8ed86e740005118667452cc821d53269e91
Ay <35792478+IveBecomeSoNumb@users.noreply.github.com>
2020-08-09 17:47:37 -0700

    a (#199)
    a (#199)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,17 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+
+        if (n == 0) {
+            return 1;
+        } else {
+            int result = n;
+            for (int i = n - 1; i > 1; i--) {
+                result *= i;
+            }
+            return result;
+        }
+
+    }
 }
