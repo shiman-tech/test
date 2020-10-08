7c44257fd522663a7803e186c094c34b9583aa85
catmmao <catmmao@gmail.com>
2020-10-08 14:20:45 +0800

    完善代码 (#204)
    完善代码 (#204)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,17 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        int result = n;
+
+        for (int i = 1; i < n; i++) {
+            result *= n - i;
+        }
+
+        return result;
+    }
 }
