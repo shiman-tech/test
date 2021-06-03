3b345f9f04b48754e92c03b8547bdab7662c2fe6
Oculta Chow <33774472+Ocultaz@users.noreply.github.com>
2021-06-03 21:06:36 +0900

    Factorial (#221)
    Factorial (#221)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,14 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        int j = 1;
+        if (n == 0) {
+            return 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            j *= i;
+        }
+        return j;
+    }
 }
