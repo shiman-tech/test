a74a23de77325b4201a2ca28c41734fa7fa0bbf9
codingories <34835258+codingories@users.noreply.github.com>
2021-09-23 20:29:27 +0800

    finish (#231)
    finish (#231)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,11 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        int j = 1;
+        for (int i = 1; i < n; i++) {
+            j = j * i;
+        }
+        return j;
+    }
 }
