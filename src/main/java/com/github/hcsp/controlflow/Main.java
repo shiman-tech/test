986cc00bca4d03275d709f0d90070fce40a2788f
小虾米 <freebufer.wang@gmail.com>
2021-10-31 05:13:49 +0800

    Loop factorial test (#238)
    Loop factorial test (#238)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,11 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        int x = 1;
+        for (int i = 1; i <= n; i++) {
+            x *= i;
+        }
+        return x;
+    }
 }
