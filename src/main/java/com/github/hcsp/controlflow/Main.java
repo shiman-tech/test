9d37c212a1c1f97ab4c9ef15b70de9d11c83358e
Gengooooo <32353324+leoleepsyche@users.noreply.github.com>
2021-11-07 01:24:12 +0000

    阶乘 (#240)
    阶乘 (#240)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,18 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        int total = 1;
+        if (n == 0) {
+            return  1;
+        } else {
+            for (int i = n; i > 1; i --) {
+                total *=i;
+            }
+        }
+
+
+        return  total;
+
+    }
 }
