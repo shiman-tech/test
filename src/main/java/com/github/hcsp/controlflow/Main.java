f3a9bc33eb1a3a50a5bd59a8bac32720e0acd1b7
yangbisheng <2606424906@qq.com>
2022-08-03 04:13:02 +0800

    loop factorial (#262)
    loop factorial (#262)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,18 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            int res = 1;
+            for (int i = 1; i <= n; i++) {
+                res *= i;
+
+
+            }
+            return res;
+        }
+
+    }
 }
