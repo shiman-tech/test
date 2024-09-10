fa65134b57a38e2086212a0d401a5341af74c18d
GJV123456 <161851858+GJV123456@users.noreply.github.com>
2024-09-10 16:21:19 +0800

    1 (#273)
    1 (#273)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,14 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        int number = 1;
+        if (n == 0) {
+            return 1;
+        }
+        while (n > 1) {
+            number *= n--;
+        }
+        return number;
+    }
 }
